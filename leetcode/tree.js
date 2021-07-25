fa842151f6c44204545366ce33758a3aeebe4d94
Syldys <syldysnya@gmail.com>
2021-07-25 16:17:12 -0400

    add new

--- leetcode/tree.js
+++ leetcode/tree.js
@@ -9,3 +9,17 @@ var pruneTree = function(root) {
     
     return (root.val === 0 && !root.left && !root.right) ? null : root
 };
+
+'98. Validate Binary Search Tree'
+
+var isValidBST = function(root, min = null, max = null) {
+    if (!root) return true;
+    
+    if (min && root.val >= min.val) return false;
+    if (max && root.val <= max.val) return false;
+    
+    let left = isValidBST(root.left, root, max);
+    let right = isValidBST(root.right, min, root);
+    
+    return left && right
+};
