ad6a48159a094499f39913a816c2aa21b923f731
Syldys <syldysnya@gmail.com>
2021-08-03 19:21:13 -0400

    add

--- leetcode/tree.js
+++ leetcode/tree.js
@@ -23,3 +23,55 @@ var isValidBST = function(root, min = null, max = null) {
     
     return left && right
 };
+
+```
+   20
+  /  \
+ 9    25
+/ \   
+5   12 
+  /  \
+ 11   14 C
+     /  \
+   13 C	 16  node.right = null node.left = null
+        /  \
+    12.5 C  13.5
+ ```
+
+ function Node(key) {
+ this.key = key;    // 12
+ this.parent = null;// 9
+ this.left = null;  // 11
+ this.right = null; // 14
+}
+
+
+'12 -> 12.5'
+'9  -> 11'
+'16 -> 20'
+'25 -> null'
+
+'12 -> check right -> check left -> check left -> check left'
+
+function nextLargestEle(node) { // node = 16
+ if (!node.parent && !node.left && !node.right) return null; // O(1)
+ 
+   if (node.right === null) {
+     return recursionParent(node.parent, node.key) // n = node height
+ }
+ 
+ return recursionChild(node.right); // n = node height -> space O(1)
+}
+
+function recursionParent(node, target) { // node = 20, target = 16
+   if (node.parent && node.parent.key > target) return node.parent.key;
+ if (node.parent === null) return null;
+ 
+ return recursion(node.parent, target);
+}
+
+function recursionChild(node) { // node = 12.5
+   if (node.left === null) return node.key; // 12.5
+ 
+ return recursion(node.left); // node = 13
+}
