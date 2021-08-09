eaec5d9baccc8be0ae9d20e7d86f595848170f3c
Syldys <syldysnya@gmail.com>
2021-08-09 14:30:32 -0400

    add

--- leetcode/array.js
+++ leetcode/array.js
@@ -1,5 +1,40 @@
-'Remove Duplicates from Sorted Array'
+const groupAnagrams = (words) => {
+	let arrOfObjects = []; // [{ yo: {y: 1, o: 1}, act: {a: 1, c: 1, t: 1}, oy: {o: 1, y: 1}]
 
-var removeDuplicates = function(nums) {
-    
-};
+	for (i = 0; i < words.length; i++) { // words[i] = oy // n = words.length
+	    let stringObj = {}; // {oy: {o: 1, y: 1}}
+        let key = words[i]; // 
+	    stringObj[key] = {}; 
+	
+	    for (j = 0; j < key.length; j++) { // key[j] = o // m = key.length
+            if (stringObj[key][key[j]]) { // stringObj[oy][o]
+                stringObj[key][key[j]] += 1
+            } else {
+                stringObj[key][key[j]] = 1
+            }
+        }
+        arrOfObjects.push(stringObj)
+    } // O(n) * O(m)
+    // [{yo: { y: 1, o: 1}}, {act: {a: 1, c: 1, t: 1}}, {oy: {o: 1, y: 1}}]
+    let anagrams = {}; // { {y: 1, o: 1}: [yo, oy], {a: 1, c: 1, t: 1}: [act],  }
+
+    for (i = 0; i < arrOfObjects.length; i++) {  // [{ yo: {y: 1, o: 1}, act: {a: 1, c: 1, t: 1}, oy: {o: 1, y: 1}] // k = arrOfObjects.length
+        let strObj = arrOfObjects[i]; // { yo: {y: 1, o: 1}
+        let k = Object.keys(strObj)[0]; // k = yo
+        let v = Object.values(strObj)[0]; // v = {y: 1, o: 1}
+        
+	    if (!anagrams[v]) {
+	        anagrams[k] = v
+        } else {
+            anagrams[k] = anagrams[v].concat([k])	
+        }
+    } // O(k)
+
+// { { y: 1, o: 1}: [yo, oy] }
+    console.log(anagrams)
+    return Object.values(anagrams)
+}
+
+const words = ['yo', 'act', 'oy'];
+console.log(groupAnagrams(words))
+
